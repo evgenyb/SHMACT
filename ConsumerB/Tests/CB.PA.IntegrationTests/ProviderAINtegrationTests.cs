56606d48b759ac5e191775180f103933f6a65e09
Evgeny Borzenin <evgeny.borzenin@if.no>
2017-12-01 20:39:41 +0100

    Added ConsumerB solution

---
+++ ConsumerB/Tests/CB.PA.IntegrationTests/ProviderAINtegrationTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using Castle.Facilities.WcfIntegration;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using ProviderA.Contracts;
+using ProviderA.Contracts.ServiceDependencies.CB;
+
+namespace CA.PA.IntegrationTests
+{
+    [TestFixture]
+    public class ProviderAIntegrationTests
+    {
+        IWindsorContainer _container;
+        private IProviderAForCB _providerb;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _container = new WindsorContainer();
+            _container.AddFacility<WcfFacility>();
+            var httpBaseUrl = ConfigurationManager.AppSettings["Global.WcfServices.HttpBaseUrl"];
+            _container.Register(Component.For<IProviderAForCB>()
+                .AsWcfClient(WcfEndpoint
+                    .BoundTo(new BasicHttpBinding())
+                    .At(httpBaseUrl + "/ProviderA/ProviderA.svc")));
+
+            _providerb = _container.Resolve<IProviderAForCB>();
+        }
+
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+
+        [Test]
+        public void IProviderAForCA_M1()
+        {
+            var response = _providerb.M2();
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response, Is.TypeOf<D2>());
+        }
+    }
+}
